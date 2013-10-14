c54be7345b9c114b404a935b60cdef7f65b0c494
syucream <syucream1031@gmail.com>
2013-10-14 23:28:04 +0900

    Add essential processes

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,24 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "libev-devel"
+
+localrepo_path = "#{Chef::Config[:file_cache_path]}/weighttp"
+giturl = "https://github.com/lighttpd/weighttp.git"
+
+git localrepo_path do
+  repository giturl
+  reference "master"
+  action :sync
+end
+
+script "install_weighttp" do
+  cwd localrepo_path
+  interpreter "bash"
+  user "root"
+  code <<-EOH
+./waf configure build install
+EOH
+end
+
